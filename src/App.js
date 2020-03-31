e78f84b14257483ffe714cbd56e1ee4b9d10b409
Romairo1987 <49536760+Romairo1987@users.noreply.github.com>
2020-03-30 21:24:57 -0700

    finished

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
+import data from './data';
+
+// Components
+import Navigation from './components/Navigation';
+import Products from './components/Products';
+import ShoppingCart from './components/ShoppingCart';
+import ProductContext from "./contexts/ProductContext"
+
+
+function App() {
+
+	const [products] = useState(data);
+	const [cart, setCart] = useState([]);
+
+	const addItem = item => {
+		// add the given item to the cart
+		setCart([...cart, item])
+	};
+
+	const removeItem = id => {
+		const deleteCartItem = cart.filter(item => item.id !== id);
+		setCart([...deleteCartItem]);
+   };
+
+	return (
+
+		<ProductContext.Provider value={{products, addItem, cart, removeItem}}>
+		<div className="App">
+			<Navigation cart={cart} />
+
+			{/* Routes */}
+			<Route
+				exact
+				path="/"
+				render={() => (
+						<Products
+							products={products}
+							addItem={addItem}
+						/>
+				)}
+			/>
+
+			<Route
+				path="/cart"
+				render={() => <ShoppingCart cart={cart}  removeItem={removeItem}/>}
+			/>
+		</div>
+	</ProductContext.Provider>
+	);
+	
+}
+
+export default App;
